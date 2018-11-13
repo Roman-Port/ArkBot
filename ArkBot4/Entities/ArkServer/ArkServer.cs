92deaf1d9c73acb37bc2339c91e053a28ad309d1
Roman-Port <darkwings220@gmail.com>
2018-11-13 08:15:55 -0600

    Initial commit

---
+++ ArkBot4/Entities/ArkServer/ArkServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArkBot4.Entities.ArkServer
+{
+    public class ArkServer
+    {
+        // SERIALIZED ARK PROPERTIES
+        public string rcon_ip { get; set; }
+        public int rcon_port { get; set; }
+        public string rcon_password { get; set; }
+
+        public string server_name { get; set; }
+    }
+}
---
+++ ArkBot4/Program.cs
@@ -0,0 +1,24 @@
+﻿using DiscordBotFrame;
+using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+
+namespace ArkBot4
+{
+    class Program
+    {
+        public static DiscordInterface discord;
+
+        static void Main(string[] args)
+        {
+            discord = new DiscordInterface("%", "", (DSharpPlus.Entities.DiscordUser u, DSharpPlus.Entities.DiscordChannel c) =>
+            {
+                return DiscordPermissionLevel.Owner;
+            });
+            
+        }
+        
+    }
+
+    
+}
